fb6027d8bf5c56a5ea053e4106667ed8b1f6eece
FilhoDoan <doangalvaofilho@gmail.com>
2022-11-25 22:39:41 -0300

    Questão 1 e 2

---
+++ src/QuestionarioAula2/Questão1.java
@@ -0,0 +1,19 @@
+package QuestionarioAula2;
+
+
+public class Questão1 {
+	
+	public static void main(String[] args) {
+		double m1,m2,m3;
+		double md;
+		
+		m1= 1.58;
+		m2 = 2.07;
+		m3= 0.55;
+		md = (m1+m2+m3)/3;
+		System.out.print(md);
+		
+	}
+	
+
+}
